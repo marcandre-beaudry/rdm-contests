0cdcb7001ff9939760393a73810437041c7ecdf5
Marc-André Beaudry <marcandre.beaudry@rci.rogers.com>
2013-05-30 16:40:59 -0400

    intitial commit

---
+++ rdm-contests.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin name : RDM Contests
+Description: This plugin will add the analytics you need to your RDM site, related to the setting provided.
+Author: Marc-Andre Beaudry and Etienne-Joseph Charles
+Version: 0.1  
+*/
+
+
